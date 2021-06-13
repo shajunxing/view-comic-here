9a0d4670383ea429be298ddd14d7004c064e52b1
shajunxing <shajunxing@163.com>
2021-06-13 06:07:29 +0000

    improved sort order

--- view_comic_here.js
+++ view_comic_here.js
@@ -32,7 +32,8 @@ async function processDir({ workingDirectory = process.cwd(), indexFilename = 'v
     console.log(arguments);
     console.log(workingDirectory, indexFilename, templateFilename);
     let template = await fs.readFile(path.join(__dirname, templateFilename), 'utf-8');
-    async function processRecur(p) {
+    let numericOrder = (a, b) => (a.length - b.length) || (a > b ? 1 : -1);
+    let processRecur = async p => {
         try {
             let dir = await fs.opendir(p);
             let subDirs = [];
@@ -42,35 +43,30 @@ async function processDir({ workingDirectory = process.cwd(), indexFilename = 'v
                 let name = dirent.name;
                 if (dirent.isDirectory()) {
                     subDirs.push(name);
-                    processRecur(path.join(p, name));
+                    processRecur(path.join(p, name)); // no await
                 } else if (dirent.isFile()) {
-                    let extl = name.match(/\.(\w+)$/)?.[0]?.toLowerCase();
-                    if ({
-                        '.jpg': true,
-                        '.jpeg': true,
-                        '.jpe': true,
-                        '.jfif': true,
-                        '.png': true,
-                        '.gif': true,
-                        '.bmp': true,
-                    }[extl]) {
-                        imgFiles.push(name)
-                    } else if ({
-                        '.htm': true,
-                        '.html': true,
-                        '.txt': true,
-                    }[extl]) {
+                    let nameParsed = path.parse(name);
+                    let extl = nameParsed.ext.toLowerCase();
+                    if ({ '.jpg': 1, '.jpeg': 1, '.jpe': 1, '.jfif': 1, '.png': 1, '.gif': 1, '.bmp': 1, }[extl]) {
+                        imgFiles.push(nameParsed);
+                    } else if ({ '.htm': 1, '.html': 1, '.txt': 1, }[extl]) {
                         if (name !== indexFilename) {
-                            miscFiles.push(name)
+                            miscFiles.push(name);
                         }
                     }
                 }
             }
+            let subDirsHtml = subDirs.sort(numericOrder)
+                .map(d => `<a href="${encodeURIComponent(d)}${path.sep}${indexFilename}">${d}${path.sep}</a>`).join('\n');
+            let imgFilesHtml = imgFiles.sort((a, b) => numericOrder(a.name, b.name))
+                .map(f => `<img src="${encodeURIComponent(f.base)}" title="${f.base}">`).join('\n');
+            let miscFilesHtml = miscFiles.sort()
+                .map(m => `<a href="${encodeURIComponent(m)}">${m}</a>`).join('\n');
             fs.writeFile(path.join(p, indexFilename), eval('`' + template + '`'), 'utf-8');
         } catch (err) {
             console.error(err);
         }
-    }
+    };
     processRecur(workingDirectory);
 }
 
